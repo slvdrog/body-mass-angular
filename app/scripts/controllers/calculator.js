e26e07da53b35c5c1d69c4aa6b99d80fa8d8d07f
Salvador <salvador.og@gmail.com>
2017-08-18 04:57:05 -0300

    New build

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -3,10 +3,12 @@
 angular.module('bodyMassApp')
   .controller('CalculatorCtrl', function ($scope, BodyMass, alertService, $state) {
     $scope.category = '';
+    $scope.bmi = '';
 
     $scope.handleBMIClick = function() {
       var height = $scope.bmiForm.height / 100
       BodyMass.save({height: height, weight: $scope.bmiForm.weight }, function(response){
+        $scope.bmi = response.bmi;
         $scope.category = response.category;
       });
     };
